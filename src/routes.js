9be9fb7576a4ba59988adfadb61b4099dbcf7c81
Derrick <riobad74@gmail.com>
2024-05-15 14:44:57 +0300

    test done

--- src/routes.js
+++ src/routes.js
@@ -1,7 +1,28 @@
-
+import Home from "./pages/Home";
+import Movie from "./pages/Movie";
+import Actors from "./pages/Actors";
+import Directors from "./pages/Directors";
+import ErrorPage from "./pages/Error";
 
 const routes = [
-   
+  {
+    path: "/",
+    element: <Home />,
+    errorElement: <ErrorPage />
+  },
+  {
+    path: "/movie/:id",
+    element: <Movie />
+  },
+  {
+    path: "/directors",
+    element: <Directors />
+  },
+  {
+    path:"/actors",
+    element: <Actors />
+  }
+  
   ];
 
 export default routes;
